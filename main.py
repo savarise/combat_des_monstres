4d3f0b01b93276bde429764a9264b57593ad95d8
savarise <143745809+savarise@users.noreply.github.com>
2023-10-02 15:31:49 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,71 @@
-def compter_mots(chaine):
-    # split() permet de separer une phrase en chaines de characteres en les separant par un espace (whitespace)
-    # on utilise donc le chaine (ce que on met sur la fonction) et on le split()
-    mots = chaine.split()
+import random
 
-    # on utlise la fonction len() pour compter le nombre de chaines de characteres qu'on a dans le chaine.split()
-    nombre_de_mots = len(mots)
+def main(): # fonction du jeu entier
+   niveau_vie = 20
+   nombre_victoires = 0
+   nombre_defaites = 0
+   nombre_victoires_consecutives = 0
+   numero_combat = 0
 
-    print("Le nombre de mots dans la chaine est :", nombre_de_mots)
-# alors, on utilise la fonction sur ce que l'utilisateur met comme input
-chaine_de_caracteres = input("Met des chaines de characteres et je vais compter combien il y en a.")
-compter_mots(chaine_de_caracteres)
+   while niveau_vie > 0:
+       force_adversaire = random.randint(1, 5*2)
+       numero_adversaire = nombre_victoires + nombre_defaites + 1
+
+       print("Vous tombez face à face avec un adversaire de difficulté :", force_adversaire)
+       print("Que voulez-vous faire ?")
+       print("1- Combattre cet adversaire")
+       print("2- Contourner cet adversaire et aller ouvrir une autre porte")
+       print("3- Afficher les règles du jeu")
+       print("4- Quitter la partie")
+
+       choix = input("Votre choix : ")
+
+       if choix == '1':
+           numero_combat += 1
+           score_des = random.randint(1, 6) + random.randint(1, 6)
+           print(f"Lancer des dés : {score_des}")
+
+           if score_des > force_adversaire:
+               combat_statut = "victoire"
+               niveau_vie += force_adversaire
+               nombre_victoires += 1
+               nombre_victoires_consecutives += 1
+               print(f"Dernier combat = {combat_statut}")
+               print(f"Niveau de vie : {niveau_vie}")
+               print(f"Nombre de victoires consecutives = {nombre_victoires_consecutives}")
+           else:
+               combat_statut = "defaite"
+               niveau_vie -= force_adversaire
+               nombre_defaites += 1
+               nombre_victoires_consecutives = 0
+               print(f"Dernier combat = {combat_statut}")
+               print(f"Niveau de vie : {niveau_vie}")
+       elif choix == '2':
+           niveau_vie -= 1
+           nombre_defaites += 1
+           nombre_victoires_consecutives = 0
+           print("Vous contournez l'adversaire, mais cela vous coûte 1 point de vie.")
+       elif choix == '3':
+           print("Pour réussir un combat, il faut que la somme des valeurs des dés lancés soit supérieure à la force de l'adversaire.")
+           print("Dans ce cas, le niveau de vie de l'usager est augmenté de la force de l'adversaire.")
+           print("Une défaite a lieu lorsque la somme des valeurs des dés lancés par l'usager est inférieure ou égale à la force de l'adversaire.")
+           print("Dans ce cas, le niveau de vie de l'usager est diminué de la force de l'adversaire.")
+           print("La partie se termine lorsque les points de vie de l'usager tombent sous 0.")
+           print("L'usager peut combattre ou éviter chaque adversaire, dans le cas de l'évitement, il y a une pénalité de 1 point de vie.")
+       elif choix == '4':
+           print("Merci et au revoir...")
+           break
+       else:
+           print("Choix invalide. Veuillez choisir une option valide.")
+
+       if nombre_victoires_consecutives == 3:
+           force_adversaire += 1
+           nombre_victoires_consecutives = 0
+
+       print(f"Adversaire : {numero_adversaire}")
+       print(f"Force de l'adversaire : {force_adversaire}")
+       print(f"Niveau de vie de l'usager : {niveau_vie}")
+       print(f"Combat {numero_combat} : {nombre_victoires} vs {nombre_defaites} \n")
+
+   print(f"La partie est terminée. Vous avez vaincu {nombre_victoires} monstre(s).")
+main()
