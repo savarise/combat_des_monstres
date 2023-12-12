abc19543762b37c060ea6f8232b74d94def0cf0d
savarise <143745809+savarise@users.noreply.github.com>
2023-12-12 11:57:41 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import random
-
-def main():
+# fait tout le jeu
+def main(): 
     niveau_vie = 20
     nombre_victoires = 0
     nombre_defaites = 0
     numero_combat = 0
     boss = 0
     boss_absent = 0
-
+    # pendant que le joueur nest pas mort, il peut continuer le jeu
     while niveau_vie > 0:
         numero_adversaire = nombre_victoires + nombre_defaites + 1
 
@@ -20,7 +20,7 @@ def main():
             boss_absent = 0
             boss = 0
             force_adversaire = random.randint(1, 5*2)
-
+    
         print("Vous tombez face à face avec un adversaire de difficulté :", force_adversaire)
         print("Que voulez-vous faire ?")
         print("1- Combattre cet adversaire")
@@ -29,19 +29,20 @@ def main():
         print("4- Quitter la partie")
 
         choix = input("Votre choix : ")
-
-        if choix == '1':
+        # choix de combattre enemi
+        if choix == '1': 
             numero_combat += 1
             score_des = random.randint(1, 6) + random.randint(1, 6)
             print(f"Lancer des dés : {score_des}")
-
-            if score_des > force_adversaire:
+            # ce qui arrive quand on gagne
+            if score_des > force_adversaire: 
                 combat_statut = "victoire"
                 niveau_vie += force_adversaire
                 nombre_victoires += 1
                 print(f"Dernier combat = {combat_statut}")
                 print(f"Niveau de vie : {niveau_vie}")
-            else:
+            # ce qui arrive quand on perd
+            else: 
                 combat_statut = "defaite"
                 niveau_vie -= force_adversaire
                 nombre_defaites += 1
@@ -49,17 +50,20 @@ def main():
                 print(f"Niveau de vie : {niveau_vie}")
                 if boss == 1:
                     boss_absent = 1
+                    
         elif choix == '2':
             niveau_vie -= 1
             nombre_defaites += 1
             print("Vous contournez l'adversaire, mais cela vous coûte 1 point de vie.")
-        elif choix == '3':
+        # affiche les regles
+        elif choix == '3': 
             print("Pour réussir un combat, il faut que la somme des valeurs des dés lancés soit supérieure à la force de l'adversaire.")
             print("Dans ce cas, le niveau de vie de l'usager est augmenté de la force de l'adversaire.")
             print("Une défaite a lieu lorsque la somme des valeurs des dés lancés par l'usager est inférieure ou égale à la force de l'adversaire.")
             print("Dans ce cas, le niveau de vie de l'usager est diminué de la force de l'adversaire.")
             print("La partie se termine lorsque les points de vie de l'usager tombent sous 0.")
             print("L'usager peut combattre ou éviter chaque adversaire, dans le cas de l'évitement, il y a une pénalité de 1 point de vie.")
+            
         elif choix == '4':
             print("Merci et au revoir...")
             break
